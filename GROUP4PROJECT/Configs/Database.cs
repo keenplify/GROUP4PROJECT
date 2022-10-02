a92cbfd26640da0425318ee1bbdf239fbe0e89b1
Aczell Florencio <keenplify@outlook.com>
2022-10-02 23:13:14 +0800

    CHORE: Added database config

---
+++ GROUP4PROJECT/Configs/Database.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+
+namespace GROUP4PROJECT.Configs
+{
+    public class Database
+    {
+        public static string GetConnectionString()
+        {
+            string host = "db.lmchozvtyhzraokprqdq.supabase.co";
+            string username = "postgres";
+            string password = "xtTNvEdWTMB7QFb4";
+            string database = "postgres";
+            return $"Host={host};Username={username};password={password};Database={database}";
+        }
+
+        public static NpgsqlConnection GetConnection()
+        {
+            string connectionString = GetConnectionString();
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
