1ba4a27e90edca03675d2ac7e647f3a2fa69de6a
Aczell Florencio <keenplify@outlook.com>
2022-10-25 17:11:44 +0800

    CU-3mzv9rz

---
+++ GROUP4PROJECT/Models/Administrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GROUP4PROJECT.Models
+{
+    public class Administrator
+    {
+        public string id { get; set; }
+        public string created_at { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string full_name { get; set; }
+    }
+}
