da00a49a6f9701f0bba54ff080530e50f49bf362
peni00 <sandiqueruben@gmail.com>
2022-10-31 18:03:01 +0800

    updated
    float for price

--- GROUP4PROJECT/Models/Product.cs
+++ GROUP4PROJECT/Models/Product.cs
@@ -9,5 +9,5 @@ namespace GROUP4PROJECT.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public float Price { get; set; }
     }
