abe61b3b46810009c25920906bec472e8252336e
Aczell Florencio <keenplify@outlook.com>
2022-11-06 11:53:23 +0800

    CU-3mzvfvq

---
+++ GROUP4PROJECT/Services/QRCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QRCoder;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace GROUP4PROJECT.Services
+{
+    public class QRCodeService
+    {
+        public string GenerateBase64(string payload,QRCodeGenerator.ECCLevel eccLevel=QRCodeGenerator.ECCLevel.Q)
+        {
+            var generator = new QRCodeGenerator();
+
+            var data = generator.CreateQrCode(payload, eccLevel);
+
+            var qrCode = new QRCode(data);
+
+            var bitmap = qrCode.GetGraphic(20);
+
+            var ms = new MemoryStream();
+
+            bitmap.Save(ms, ImageFormat.Jpeg);
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+    }
+}
