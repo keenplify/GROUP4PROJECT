68feeddd7c458fefbcc87d34d4d435109240d21c
John Renzo M. Crisostomo <johnrenzo.crisostomo07@gmail.com>
2022-11-06 20:37:42 +0800

    Revert "Revert "POS AND ADMIN""
    This reverts commit 93a9926682a0739186d3f4d8ddc1c6a15d867028.

--- GROUP4PROJECT/Controllers/AdminController.cs
+++ GROUP4PROJECT/Controllers/AdminController.cs
@@ -17,5 +17,14 @@ namespace GROUP4PROJECT.Controllers
         {
             return View();
         }
+
+        public ActionResult LoginAdmin()
+        {
+            return View();
+        }
+        public ActionResult EmployeeAdmin()
+        {
+            return View();
+        }
     }
 }
