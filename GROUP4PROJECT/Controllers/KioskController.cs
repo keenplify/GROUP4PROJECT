c3df67a63297de4995d61e6c27958955fc854592
Aczell Florencio <keenplify@outlook.com>
2022-11-08 17:54:13 +0800

    Kiosk and Barista Integrations

---
+++ GROUP4PROJECT/Controllers/KioskController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GROUP4PROJECT.Controllers
+{
+    public class KioskController : Controller
+    {
+        // GET: Kiosk
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult Receipt()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
