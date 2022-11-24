adca45ee54a8a586fdd88657eb1be038fb903802
Aczell Florencio <keenplify@outlook.com>
2022-11-25 03:13:38 +0800

    #3mzvg4q

---
+++ GROUP4PROJECT/Scripts/kiosk-cart.js
@@ -0,0 +1,77 @@
+﻿let container;
+let totalItemsElement;
+let totalPriceElement;
+
+function init() {
+    container = document.querySelector("#cart-container");
+    totalItemsElement = document.querySelector("#cart-total-items");
+    totalPriceElement = document.querySelector("#cart-total-price");
+
+    updateCartView();
+}
+
+function removeFromCart(id) {
+    let cartItems = tryParseLocalStorageObject("kioskItems");
+
+    if (!cartItems) cartItems = [];
+
+    const cartIndex = cartItems.findIndex(v => v.id == id);
+
+    if (cartIndex != -1) cartItems.splice(cartIndex, 1);
+
+    trySaveLocalStorageObject("kioskItems", cartItems);
+    updateCartView();
+}
+
+async function updateCartView() {
+    const kioskItems = tryParseLocalStorageObject("kioskItems");
+
+    if (!kioskItems) return;
+
+    let html = "";
+    totalPrice = 0;
+    let totalItems = 0;
+    for (const cart of kioskItems) {
+        const product = await $.ajax(`/Products/Single?id=${cart.id}`, {
+            cache: true
+        }).promise();
+        const price = (product.Price * cart.quantity)
+        totalPrice += price;
+        totalItems += cart.quantity;
+
+        html += `
+            <tr>
+                <td><img class="d-block mx-auto" src="${product.ImageUrl}" width="125"></td>
+                <td>${product.Name}</td>
+                <td>${cart.remarks ?? 'N/A'}</td>
+                <td>x${cart.quantity}</td>
+                <td>${product.Price}</td>
+                <td>${price}</td>
+                <td>
+                    <button class="btn btn-ghost" onclick="removeFromCart('${product.Id}')">
+                        <img
+                            src="/Kiosk/icon/Kiosk_Cart-Remove.svg"
+                            width="40"
+                            ondragstart="return false;"
+                        >
+                    </button>
+                </td>
+            </tr>
+        `;
+    }
+
+    container.innerHTML = html;
+    totalPriceElement.innerHTML = `₱${totalPrice}`;
+    totalItemsElement.innerHTML = totalItems;
+}
+
+async function generateTicket() {
+    const customerName = document.querySelector("#customer-name");
+    if (customerName.value && customerName.length > 0) {
+        localStorage.setItem("kioskCustomerName", customerName.value)
+    }
+
+    window.location.href = "/Kiosk/Receipt"
+}
+
+document.onload = init();
---
+++ GROUP4PROJECT/Scripts/kiosk.js
@@ -0,0 +1,52 @@
+﻿function setup(type) {
+    localStorage.setItem("orderType", type);
+    localStorage.removeItem("kioskItems");
+    localStorage.removeItem("kioskCustomerName");
+
+    window.location.href = "/Kiosk/Menu";
+}
+
+function showProductModal(json) {
+    const modal = document.querySelector("#product-modal");
+    const product = JSON.parse(json);
+    document.querySelector("#product-name").innerHTML = product.Name;
+    document.querySelector("#product-price").innerHTML = product.Price;
+    document.querySelector("#product-description").innerHTML = product.Description;
+    document.querySelector("#product-image").src = product.ImageUrl;
+    document.querySelector("#product-id").value = product.Id;
+
+    new bootstrap.Modal(modal).show()
+}
+
+function addToCart() {
+    const id = document.querySelector("#product-id")?.value;
+
+    if (!id) {
+        return Toastify({
+            text: "Unable to add to cart",
+        }).showToast();
+    }
+
+    let cartItems = tryParseLocalStorageObject("kioskItems");
+
+    if (!cartItems) cartItems = [];
+
+    const oldCart = cartItems.find(v => v.id == id);
+
+    const remarks = document.querySelector("#remarks");
+    console.log(remarks.value);
+
+    if (oldCart) oldCart.quantity++;
+    else cartItems.push({
+        id,
+        quantity: 1,
+        remarks: remarks.value?.length > 0 ? remarks.value : undefined 
+    })
+
+    trySaveLocalStorageObject("kioskItems", cartItems);
+    remarks.value = "";
+
+    return Toastify({
+        text: "Product added to cart.",
+    }).showToast();
+}
